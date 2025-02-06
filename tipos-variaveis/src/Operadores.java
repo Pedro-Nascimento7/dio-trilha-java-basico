86f5f72b46591803a17438b86cabf5c9d7a5d330
Pedro-Nascimento7 <pedro077ramos@gmail.com>
2025-02-06 19:51:05 -0300

    atualizando

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args) {
+    boolean condicao1 = true;
+    boolean condicao2 = false;
+
+    if (condicao1 && condicao2) {
+        System.out.println("As duas condições são verdadeiras");
+    }
+    if (condicao1 || condicao2) {
+        System.out.println("uma das condições são verdadeiras");
+    }
+        System.out.println("Fim");
+
+    }
+}
