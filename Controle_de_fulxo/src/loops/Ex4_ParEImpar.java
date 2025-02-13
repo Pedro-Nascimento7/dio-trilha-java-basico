88d7ae5150f04125ca625e33064319575c3d56cb
Pedro-Nascimento7 <pedro077ramos@gmail.com>
2025-02-13 18:07:56 -0300

    Ex4_Par_e_Impar

--- Controle_de_fulxo/src/loops/Ex4_ParEImpar.java
+++ Controle_de_fulxo/src/loops/Ex4_ParEImpar.java
@@ -1,8 +1,36 @@
 package loops;
+
+import java.util.Scanner;
+
 /*Faça um programa que peça N números inteiros,
  * calcule e mostre a quantidade de números pares
  * e a quantidade de números impares.
  */
 public class Ex4_ParEImpar {
-    
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        int quantNumeros;
+        int numero;
+        int quantidadePares = 0, quantidadeImpares = 0;
+
+        System.out.println("Quantidade de números");
+        quantNumeros = scan.nextInt();
+        int count = 0;
+        do {
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+
+            if (numero % 2 == 0)  quantidadePares++;
+                else quantidadeImpares++;
+            
+
+            count++;
+
+        } while(count < quantNumeros);
+
+        System.out.println("Quantidade Par: "+ quantidadePares);
+        System.out.println("Quantidade Impar: "+ quantidadeImpares);
+    }
 }
