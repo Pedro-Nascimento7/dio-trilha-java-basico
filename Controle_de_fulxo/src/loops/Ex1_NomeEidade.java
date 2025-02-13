366887ad97d61752b38d4aafd9e087db85d9b528
Pedro-Nascimento7 <pedro077ramos@gmail.com>
2025-02-13 14:28:52 -0300

    loop

---
+++ Controle_de_fulxo/src/loops/Ex1_NomeEidade.java
@@ -0,0 +1,29 @@
+package loops;
+
+import java.util.Scanner;
+
+/*Faça um programa que leia conjuntos de doi valores,
+ * o primeiro representando o nome do aluno e o segundo representando sua idade.
+ * (pere o programa inserindo o valor 0 no campo nome)
+ */
+public class Ex1_NomeEidade {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while (true) {
+            System.out.println("Nome: ");
+            nome = scan.next();
+            if (nome.equals("0")) break ;
+                           
+              System.out.println("Idade: ");
+            idade = scan.nextInt();
+            
+            
+        }
+        System.out.println("Continua aqui...");
+
+    }
+}
